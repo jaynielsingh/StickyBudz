497a7e57d7f22defc58c05ac0514111ce96af105
Jayniel Singh <jaynielsingh@yahoo.com>
2020-11-03 15:36:09 -0800

    Initial Commit

---
+++ StickyBudz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StickyBudz
+//
+//  Created by Jayniel on 11/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
